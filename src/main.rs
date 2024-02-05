ed6755051ebd4cb8e4efd1523576c638fc473187
lulalulo <lulalulo@tutanota.com>
2024-02-05 15:53:04 -0700

    removed unused imports

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{web, App, HttpServer, Responder};
 
 async fn index() -> impl Responder {
     "Hello World!"
