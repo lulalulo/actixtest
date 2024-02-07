ffdb6eed856f172809557a523e0145453a96de66
lulalulo <lulalulo@tutanota.com>
2024-02-06 21:17:38 -0700

    struct and index fn

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,13 @@
 use actix_web::{get, web, App, HttpServer};
 
+// This represents state
+struct AppState {
+    app_name: String,
+}
+
+#[get("/")]
+async fn index(data: web::Data<AppState>) -> String {
+    let app_name = &data.app_name; //GET app_name
+    format!("Hello {app_name}") //response with app_name
+}
+
