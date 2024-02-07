2a7f7c94ba94ce42224c2e3c644c6dd0e0ac8148
lulalulo <lulalulo@tutanota.com>
2024-02-06 21:21:40 -0700

    main

--- src/main.rs
+++ src/main.rs
@@ -11,3 +11,16 @@ async fn index(data: web::Data<AppState>) -> String {
     format!("Hello {app_name}") //response with app_name
 }
 
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpSErver::new(|| {
+        App::new()
+            .app_data(web::Data::new(AppState {
+                app_name: String::from("Actix Web"),
+            }))
+            .service(index)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
