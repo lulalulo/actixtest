c978318bbd157df7fe8f00a9d1ba7136e94046fd
lulalulo <lulalulo@tutanota.com>
2024-02-06 21:15:19 -0700

    state first

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,2 @@
-use actix_web::{web, App, HttpServer, Responder};
+use actix_web::{get, web, App, HttpServer};
 
-async fn index() -> impl Responder {
-    "Hello World!"
-}
-
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new().service(
-            // prefixes all resources and routes attached to it
-            web::scope("/app")
-                // this handles request for 'GET /app/index.html'
-                .route("/index.html", web::get().to(index)),
-        )
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
-}
