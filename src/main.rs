8e32f59c515da4f8d1be38470138c71afce1e335
lulalulo <lululo@tutanota.com>
2024-02-09 21:00:36 -0700

    json cfg example

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use actix_web::{get, web, App, HttpServer, Result};
+use actix_web::{error, web, App, HttpResponse, HttpServer, Responder};
 use serde::Deserialize;
 
 #[derive(Deserialize)]
@@ -6,23 +6,29 @@ struct Info {
     username: String,
 }
 
-/// deserialize info from request body
-#[post("/submit")]
-async fn submit(info: web::Json<Info>) -> Result<String> {
-    Ok(format!("Welcome {}!", info.username))
-}
-
-/// extract path info using serde
+/// deserialize info from request body, max payload size is 4kb
 #[get("/")] // <- define path params
-async fn index(info: web::Query<info>) -> String {
+async fn index(info: web::Query<info>) -> impl Responder {
     format!("Welcome {}!", info.username)
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    use actix_web::{App, HttpServer};
-
-    HttpServer::new(|| App::new().service(index))
+    HttpServer::new(|| {
+            let json_config = web::JsonConfig::default()
+                .limit(4096)
+                .error_handler(|err, _req| {
+                    // create custome error responde
+                    error::InternalError::from_response(err, HttpResponde::Conflict().finish())
+                        .into
+                });
+            App::new().service(
+                web::resource("/")
+                    // change json extractor configurator
+                    .app_data(json_config)
+                    .route(web::post().to(index)),
+            )
+        })
         .bind(("127.0.0.1", 8080))?
         .run()
         .await
