07056b661b2bb5f011d8e214eeaeddde9501d0ef
lulalulo <lululo@tutanota.com>
2024-02-09 20:49:11 -0700

    real query index example

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,15 @@
-use actix_web::{get, web, Result};
+use actix_web::{get, web, App, HttpServer};
 use serde::Deserialize;
 
 #[derive(Deserialize)]
 struct Info {
-    user_id, u32,
-    friend: String,
+    username: String,
 }
 
 /// extract path info using serde
-#[get("/users/{users_id}/{friend}")] // <- define path params
-async fn index(req: HttpRequest) -> Result<String> {
-    let name: String = req.match_info().get("friend").unwrap().parse().unwrap();
-    let userid: i32 = req.match_info().query("user_id").parse().unwrap();
-
-    Ok(format!("Welcome {}, user_id {}", name, user_id))
+#[get("/")] // <- define path params
+async fn index(info: web::Query<info>) -> String {
+    format!("Welcome {}!", info.username)
 }
 
 #[actix_web::main]
