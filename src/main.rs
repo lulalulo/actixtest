dcd53c0e697540944258e687e9667b01b089b0df
lulalulo <lululo@tutanota.com>
2024-02-11 15:23:57 -0700

    middleware compression at main

--- src/main.rs
+++ src/main.rs
@@ -5,4 +5,14 @@ async fn index() -> HttpResponse {
     HttpResponse::Ok().body("data")
 }
 
-
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .wrap(middleware::Compress::default())
+            .service(index)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
