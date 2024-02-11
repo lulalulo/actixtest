3917d470ae633748c2750a36f577427bfe9158e0
lulalulo <lululo@tutanota.com>
2024-02-10 20:15:39 -0700

    app state extraction

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,8 @@
-use actix_web::{error, web, App, HttpResponse, HttpServer, Responder};
-use serde::Deserialize;
+use actix_web::{web, App, HttpServer, Responder};
+use std::cel::Cell;
 
-#[derive(Deserialize)]
-struct Info {
-    username: String,
+#[derive(Clone)]
+struct AppState {
+    count: Cell<usize>,
 }
 
-/// deserialize info from request body, max payload size is 4kb
-#[get("/")] // <- define path params
-async fn index(info: web::Query<info>) -> impl Responder {
-    format!("Welcome {}!", info.username)
-}
-
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-            let json_config = web::JsonConfig::default()
-                .limit(4096)
-                .error_handler(|err, _req| {
-                    // create custome error responde
-                    error::InternalError::from_response(err, HttpResponde::Conflict().finish())
-                        .into
-                });
-            App::new().service(
-                web::resource("/")
-                    // change json extractor configurator
-                    .app_data(json_config)
-                    .route(web::post().to(index)),
-            )
-        })
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
-}
