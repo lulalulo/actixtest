0fd85b726506f6d3c867ef479494d85b5dadafa8
lulalulo <lululo@tutanota.com>
2024-02-11 15:21:47 -0700

    content encoding

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,8 @@
-use actix_web::{get, web, Responder, Result};
-use serde::Serialize;
+use actix_web::{get, middleware, App, HttpResponse, HttpServer};
 
-#[derive(Serialize)]
-struct MyObj {
-    name: String,
+#[get("/")]
+async fn index() -> HttpResponse {
+    HttpResponse::Ok().body("data")
 }
 
-#[get("/a/{name}")]
-async fn index(name: web::Path<String>) -> Result<impl Responder> {
-    let obj = MyObj {
-        name: name.to_string(),
-    };
-    Ok(web::Json(obj))
-}
 
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    use actix_web::{App, HttpServer};
-
-    HttpServer::new(|| App::new().service(index))
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
-}
