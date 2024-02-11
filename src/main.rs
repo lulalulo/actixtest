21d418cd656098ed5ef4c58841fd6ceedde593a7
lulalulo <lululo@tutanota.com>
2024-02-11 15:01:49 -0700

    json request exmp

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,7 @@
-use actix_web::{get, App, HttpRequest, HttpServer, Responder};
+use actix_web::{web, App, HttpServer, Result};
+use serde::Deserialize;
 
-#[get("/")]
-async fn index(req: HttpRequest) -> impl Responder {
-    let url = req.url_for("youtube", ["oHg5SJYRHA0"]).unwrap();
-    assert_eq!(url.as_str(), "https://youtube.com/watch/oHg5SJYRHA0");
-
-    url.to_string()
-}
-
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-   HttpServer::new(|| {
-        App::new()
-            .service(index)
-            .external_resource("youtube", "https://youtube.com/watch/{video_id}")
-   }) 
-   .bind(("127.0.0.1", 8080))?
-   .run()
-   .await
+#[derive(Deserialize)]
+struct info {
+    username: String,
 }
