6b67d8c5daa376af2667ae16448e4fa945ec411d
lulalulo <lululo@tutanota.com>
2024-02-11 15:56:14 -0700

    accesssing session data

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,15 @@ use actix_session::{Session, SessionMiddleware, storage::CookieSessionStore};
 use actix_web::{web, App, Error, HttpResponse, HttpServer, cookie::Key};
 
 async fn index(session: Session) -> Result<HttpResponse, Error> {
+    // access session data
+    if let Some(count) = session.get::<i32>("counter")? {
+        session.insert("counter", count + 1)?;
+    } else {
+        session.insert("counter", 1)?;
+    }
 
+    Ok(HttpResponse::Ok().body(format!(
+        "Count is {:?}!",
+        session.get::<i32>("counter")?.unwrap()
+    )))
 }
