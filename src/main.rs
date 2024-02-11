793ccca6be8a2490d4ea53c3b359eba3105c8453
lulalulo <lululo@tutanota.com>
2024-02-11 14:33:22 -0700

    main fn

--- src/main.rs
+++ src/main.rs
@@ -18,3 +18,21 @@ async fn index() -> Result<&'static str, MyError> {
     Err(err)
 }
 
+#[rustfmt::skip]
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    std::env::set_var("RUST_LOG", "info");
+    std::env::set_var("RUST_BACKTRACE", "1");
+    env_logger::init();
+
+    HttpServer::new(|| {
+        let logger = Logger::default();
+
+        App::new()
+            .wrap(logger)
+            .service(index)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
