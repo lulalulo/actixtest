8f432912c0f3839d6dfdc8caaa0833541b787e1b
lulalulo <lululo@tutanota.com>
2024-02-10 20:19:55 -0700

    add one fn

--- src/main.rs
+++ src/main.rs
@@ -9,3 +9,15 @@ struct AppState {
 async fn show_count(data: web::Data<AppState>) -> impl Responder {
     format!("count: {}", data.countget())
 }
+
+async fn add_one(data: web::Data<AppState>) -> impl Responder {
+    let count = data.count.get();
+    data.count.set(count + 1);
+
+    format!("count: {}", data.count.get())
+}
+
+#[actix_web::main]
+async fn main -> std::io::Result<()> {
+
+}
