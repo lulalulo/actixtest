6be130f077fb143bd185d672675d18741fc19689
lulalulo <lululo@tutanota.com>
2024-02-11 15:14:23 -0700

    json response exmp

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,10 @@
-use actix_web::{web, App, HttpServer, Result};
-use serde::Deserialize;
+use actix_web::{http::header::ContentType, HttpResponse};
+use serde::Serialize;
 
-#[derive(Deserialize)]
-struct Info {
-    username: String,
+#[derive(Serialize)]
+struct MyObj {
+    name: String,
 }
+async fn index() {
 
-/// extract info using serde
-async fn index(info: web::Json<Info>) -> Result<String> {
-    Ok(format!("Welcome {}!", info.username))
-}
-
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().route("/", web::post().to(index)))
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
 }
