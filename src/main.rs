7b3a2718e8368492f96a4cd9f661e3ed24001862
lulalulo <lululo@tutanota.com>
2024-02-11 15:27:06 -0700

    content encoding w identity value

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,12 @@
-use actix_web::{get, middleware, App, HttpResponse, HttpServer};
+use actix_web::{
+    get, http::header::ContentEncoding, middleware, App, HttpResponse, HttpServer,
+};
 
 #[get("/")]
 async fn index() -> HttpResponse {
-    HttpResponse::Ok().body("data")
+    HttpResponse::Ok()
+        // v- disable compression
+        .inser_header(ContentEncoding::Identity)
+        .body("data")
 }
 
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .wrap(middleware::Compress::default())
-            .service(index)
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
-}
