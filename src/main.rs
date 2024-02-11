512f9dd8dd63e17a5cdf038011b58f92dc2ab7a1
lulalulo <lululo@tutanota.com>
2024-02-10 20:32:29 -0700

    add one fn rmd

--- src/main.rs
+++ src/main.rs
@@ -19,3 +19,17 @@ async fn show_count(data: web::Data<AppState>) -> impl Responder {
         data.local_count.get()
     )
 }
+
+#[get("/add")]
+async fn add_one(data: web::Data<AppState>) -> impl Responder {
+    data.global_count.fetch_add(1, Ordering::Relaxed);
+
+    let local_count = data.local_count.get();
+    data.local_count.set(local_count + 1);
+
+    format!(
+        "global_count: {}\nlocal_count: {}",
+        data.global_count.load(Ordering::Relaxed),
+        data.local_count.get()
+    )
+}
