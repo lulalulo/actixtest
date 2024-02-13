1d8a8054ba2f805b3744e173ae8ff4cf37230b7c
lulalulo <lululo@tutanota.com>
2024-02-12 23:37:32 -0700

    static files

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,9 @@
-use actix_session::{Session, SessionMiddleware, storage::CookieSessionStore};
-use actix_web::{web, App, Error, HttpResponse, HttpServer, cookie::Key};
+use actix_files::NamedFile;
+use actix_web::HttpRequest;
+use std::path::PathBuf;
 
-async fn index(session: Session) -> Result<HttpResponse, Error> {
-    // access session data
-    if let Some(count) = session.get::<i32>("counter")? {
-        session.insert("counter", count + 1)?;
-    } else {
-        session.insert("counter", 1)?;
-    }
-
-    Ok(HttpResponse::Ok().body(format!(
-        "Count is {:?}!",
-        session.get::<i32>("counter")?.unwrap()
-    )))
+async fn index(req: HttpRequest) -> actix_web::Result<NamedFile> {
+    let path: PathBuf = req.match_info().query("filename").parse().unwrap();
+    Ok(NamedFile::open(path)?)
 }
 
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .wrap(
-                // create cookie based session middleware
-                SessionMiddleware::builder(CookieSessionStore::default(), Key::from(&[0; 64]))
-                    .cookie_secure(false)
-                    .build()
-            )
-            .service(web::resource("/").to(index))
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
-}
