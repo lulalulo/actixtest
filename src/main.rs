e739a19dd2c64c785e8b26617382f3fc6611c3c1
lulalulo <lululo@tutanota.com>
2024-02-12 23:39:40 -0700

    file dir config

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
 use actix_files as fs;
-use actix_web::{App, HttpServer};
+use actix_web::http::header::{ContentDisposition, DispositionType};
+use actix_web::{get, App, Error, HttpRequest, HttpServer};
 
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(fs::Files::new("/static", ".").show_files_listing()))
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+#[get("/{filename:.*}")]
+async fn index(req: HttpRequest) -> Result<fs::NamedFile, Error> {
+    let path: std::path::PathBuf = req.match_info().query("filename").parse().unwrap();
+    let file = fs::NamedFile::open(path)?;
+    Ok(file
+        .use_last_modified(true)
+        .set_content_disposition(ContentDisposition {
+            disposition: DispositionType::Attachment,
+            parameters: vec![],
+        }))
 }
+
