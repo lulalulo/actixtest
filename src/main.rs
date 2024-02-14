dd6898a2542755f1e0319b1b5c6988cd904c9511
lulalulo <lululo@tutanota.com>
2024-02-13 22:23:27 -0700

    websocket handler

--- src/main.rs
+++ src/main.rs
@@ -9,3 +9,15 @@ impl Actor for MyWs {
     type Context = ws::WebsocketContext<Self>;
 }
 
+/// Handler for ws::Message message
+impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for MyWs {
+    fn handle(&mut self, msg: Result<ws::Message, ws::ProtocolError>, ctx: &mut Self::Context) {
+        match msg {
+            Ok(ws::Message::Ping(msg)) => ctx.pong(&msg),
+            Ok(ws::Message::Text(text)) => ctx.text(text),
+            Ok(ws::Message::Binary(bin)) => ctx.binary(bin),
+            _ => (),
+        }
+    }
+}
+
