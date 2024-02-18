cbb78b039f61b38c2dfc91af5b4f21b4dd6f7fc2
lulalulo <lululo@tutanota.com>
2024-02-18 14:26:33 -0700

    create self signed cert/keys comment

--- src/main.rs
+++ src/main.rs
@@ -4,3 +4,11 @@ use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod};
 async fn index(_req: HttpRequest) -> impl Responder {
     "Hello."
 }
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    //Load tls keys 
+    // to create self-signed temporary cert for testing:
+    // `openssl req -x509 -newkey rsa:4096 -nodes -keyout key.pem -out cert.pem -days 365 -subj '/CN=localhost'`
+
+}
