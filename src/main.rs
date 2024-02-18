b1635e5c3c3416774bc34671bac4dd57db1ed806
lulalulo <lululo@tutanota.com>
2024-02-18 14:32:04 -0700

    http server with ssl

--- src/main.rs
+++ src/main.rs
@@ -10,5 +10,14 @@ async fn main() -> std::io::Result<()> {
     //Load tls keys 
     // to create self-signed temporary cert for testing:
     // `openssl req -x509 -newkey rsa:4096 -nodes -keyout key.pem -out cert.pem -days 365 -subj '/CN=localhost'`
+    let mut builder = SslAcceptor::mozilla_intermediate(SslMethod::tls()).unwrap();
+    builder
+        .set_private_key_file("key.pem", SslFiletype::PEM)
+        .unwrap();
+    builder.set_certificate_chain_file("cert.pem").unwrap();
 
+    HttpServer::new(|| App::new().route("/", web::get().to(index)))
+        .bind_openssl("127.0.0.1", builder)?
+        .run().
+        await
 }
